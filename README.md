997882c95a1ea82ebbb966176d6a7f80fd938d33
Alexey Avdeev <aad.jerry@gmail.com>
2017-02-17 09:31:29 +0300

    Add badge

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # vanilla-sharing
+
+[![Build Status](https://travis-ci.org/avdeev/vanilla-sharing.svg?branch=master)](https://travis-ci.org/avdeev/vanilla-sharing)
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 const WIN_PARAMS = 'scrollbars=0, resizable=1, menubar=0, left=100, top=100, width=550, height=440, toolbar=0, status=0';
 
 const vk = (options = {}) => {
