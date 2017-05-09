440dc832d43d75b095613d6c8ebe7fd7bb4cbbda
William Wong Garay <willywg@gmail.com>
2017-05-08 19:13:01 -0500

    #2 Add Linkedin on import files

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import tw from 'sharers/tw';
 import viber from 'sharers/viber';
 import vk from 'sharers/vk';
 import whatsapp from 'sharers/whatsapp';
+import linkedin from 'sharers/linkedin';
 
 export {
   fb,
@@ -18,4 +19,5 @@ export {
   viber,
   vk,
   whatsapp,
+  linkedin,
 };
