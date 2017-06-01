fed4e64e46bde6508c921e931a3e295eef6a655a
Mikhail Khazov <mkhazov.work@gmail.com>
2017-06-01 11:01:41 +0300

    Viber Share Button

--- src/sharers/viber.js
+++ src/sharers/viber.js
@@ -3,11 +3,13 @@ import encodeParams from 'utils/encodeParams';
 
 export default function viber(options = {}) {
   const { url, title } = options;
+  if (!url && !title) {
+    throw new Error('url and title not specified');
+  }
 
   const params = encodeParams({
     text: [title, url].filter(item => item).join(' '),
-    fallback: 'https://viber.com',
   });
 
-  return window.open(`https://app.adjust.com/u783g1_kw9yml?${params}`, '_blank', WIN_PARAMS);
+  return window.open(`viber://forward?${params}`, '_blank', WIN_PARAMS);
 }
