2ac62e128ccffd93ea2e01d63357c61fd63c92fe
Josh Arnold <josharnold.sv@gmail.com>
2018-02-02 16:36:17 -0500

    fix for travis

--- src/sharers/reddit.js
+++ src/sharers/reddit.js
@@ -3,14 +3,8 @@ import encodeParams from '../utils/encodeParams';
 
 export default function reddit(options = {}) {
   const { url } = options;
-
-  if (!url) {
-    throw new Error('url is not defined');
-  }
   
-  const params = encodeParams({
-    url
-  });
+  const params = encodeParams({ url });
 
   return window.open(`https://www.reddit.com/submit?${params}`, '_blank', WIN_PARAMS);
 }
