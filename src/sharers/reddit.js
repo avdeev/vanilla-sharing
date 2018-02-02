738e50a3270d5336b2767428a02d5b5bbdf919ca
Josh Arnold <josharnold.sv@gmail.com>
2018-02-02 16:42:43 -0500

    trailing space fix?

--- src/sharers/reddit.js
+++ src/sharers/reddit.js
@@ -3,7 +3,6 @@ import encodeParams from '../utils/encodeParams';
 
 export default function reddit(options = {}) {
   const { url } = options;
-  
   const params = encodeParams({ url });
 
   return window.open(`https://www.reddit.com/submit?${params}`, '_blank', WIN_PARAMS);
