15d947d03eeee114a698ab71d6c5199bef88e144
Josh Arnold <josharnold.sv@gmail.com>
2018-02-02 16:11:08 -0500

    Add tests for reddit.js

---
+++ src/sharers/tests/reddit.test.js
@@ -0,0 +1,23 @@
+import faker from 'faker';
+
+import reddit from '../reddit';
+
+describe('reddit', () => {
+  beforeEach(() => {
+    window.open = jest.fn();
+  });
+
+  it('should call without params', () => {
+    reddit();
+
+    expect(window.open.mock.calls[0][0]).toBe('https://www.reddit.com/submit?');
+  });
+
+  it('should call with url', () => {
+    const fixture = faker.internet.url();
+
+    reddit({ url: fixture });
+
+    expect(window.open.mock.calls[0][0]).toBe(`https://www.reddit.com/submit?url=${encodeURIComponent(fixture)}`);
+  });
+});
