8f123466c4c42d46f69d14f6a7a6755e830da277
Prateek Rastogi <prateek.rastogi@timesinternet.in>
2018-06-21 16:31:06 +0530

    added pinterest, tumblr sharing, and extra fields on reddit and twitter sharing. Also, updated package.json and demo index.html file

---
+++ src/sharers/tests/tumblr.test.js
@@ -0,0 +1,23 @@
+import faker from 'faker';
+
+import tumblr from '../tumblr';
+
+describe('tumblr', () => {
+  beforeEach(() => {
+    window.open = jest.fn();
+  });
+
+  it('should call without params', () => {
+    tumblr();
+
+    expect(window.open.mock.calls[0][0]).toBe('https://www.tumblr.com/widgets/share/tool?');
+  });
+
+  it('should call with url', () => {
+    const fixture = faker.internet.url();
+
+    tumblr({ url: fixture });
+
+    expect(window.open.mock.calls[0][0]).toBe(`https://www.tumblr.com/widgets/share/tool?canonicalUrl=${encodeURIComponent(fixture)}`);
+  });
+});
--- src/sharers/tw.js
+++ src/sharers/tw.js
@@ -2,11 +2,12 @@ import { WIN_PARAMS } from '../config';
 import encodeParams from '../utils/encodeParams';
 
 export default function tw(options = {}) {
-  const { title, url } = options;
+  const { title, url, hashtags = [] } = options;
 
   const params = encodeParams({
     text: title,
     url,
+    hashtags: hashtags.join(','),
   });
 
   return window.open(`https://twitter.com/intent/tweet?${params}`, '_blank', WIN_PARAMS);
