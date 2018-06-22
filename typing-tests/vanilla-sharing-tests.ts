ceee089efd2678acaca1e66e2fba1195bfdc7862
Alexey Avdeev <aad.jerry@gmail.com>
2018-06-22 13:33:12 +0300

    fix: 1.5 KB

--- typing-tests/vanilla-sharing-tests.ts
+++ typing-tests/vanilla-sharing-tests.ts
@@ -4,7 +4,7 @@ import VanillaSharing = require('../vanilla-sharing');
 const META: any = {
   URL: 'https://alexey-avdeev.com/vanilla-sharing/',
   TITLE: 'vanilla-sharing demo',
-  DESCRIPTION: 'Small (1.4 KB) simple tool for sharing url, title, description and image to VK, Facebook (Dialog, Feed, Messenger, Button), OK, G+, Twitter, Mail, LinkedIn, Line.',
+  DESCRIPTION: 'Small (1.5 KB) simple tool for sharing url, title, description and image to VK, Facebook (Dialog, Feed, Messenger, Button), OK, G+, Twitter, Mail, LinkedIn, Line.',
   IMAGE: 'https://dummyimage.com/1200x630/000/fff.jpg&text=vanilla-sharing',
   FB_APP_ID: '150778729046724',
 };
