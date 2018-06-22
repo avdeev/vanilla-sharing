0cad52c1ea8f42d02b99e55322182409fc5bc269
Alexey Avdeev <aad.jerry@gmail.com>
2018-06-22 14:51:38 +0300

    chore: upgrade eslint-config-airbnb-base to 13

--- src/utils/mobileShare.js
+++ src/utils/mobileShare.js
@@ -1,4 +1,4 @@
-import isMobileSafari from '../utils/isMobileSafari';
+import isMobileSafari from './isMobileSafari';
 
 export default function mobileShare(link) {
   return isMobileSafari() ? window.open(link) : window.location.assign(link);
