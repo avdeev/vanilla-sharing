6e80c81f9fd02a81a80178ad2150b9dd9d42a1cb
Alexey Avdeev <aad.jerry@gmail.com>
2018-06-22 14:44:27 +0300

    feat: add typings (#31)
    * feat: add typings
    
    * feat: improve tsc definitions, add typescript-test
    
    * fix: 1.5 KB

---
+++ typing-tests/vanilla-sharing-tests.ts
@@ -0,0 +1,52 @@
+/// <reference path="../vanilla-sharing.d.ts" />
+import VanillaSharing = require('../vanilla-sharing');
+
+const META: any = {
+  URL: 'https://alexey-avdeev.com/vanilla-sharing/',
+  TITLE: 'vanilla-sharing demo',
+  DESCRIPTION: 'Small (1.5 KB) simple tool for sharing url, title, description and image to VK, Facebook (Dialog, Feed, Messenger, Button), OK, G+, Twitter, Mail, LinkedIn, Line.',
+  IMAGE: 'https://dummyimage.com/1200x630/000/fff.jpg&text=vanilla-sharing',
+  FB_APP_ID: '150778729046724',
+};
+
+let window: any;
+
+window = VanillaSharing.email();
+
+window = VanillaSharing.fbButton();
+
+window = VanillaSharing.fbFeed({
+  url: META.URL,
+  redirectUri: META.URL,
+  fbAppId: META.FB_APP_ID,
+});
+
+window = VanillaSharing.fbShare();
+
+window = VanillaSharing.gp();
+
+window = VanillaSharing.line();
+
+window = VanillaSharing.linkedin();
+
+window = VanillaSharing.mail();
+
+window = VanillaSharing.messenger();
+
+window = VanillaSharing.ok();
+
+window = VanillaSharing.pinterest();
+
+window = VanillaSharing.reddit();
+
+window = VanillaSharing.telegram();
+
+window = VanillaSharing.tumblr();
+
+window = VanillaSharing.tw();
+
+window = VanillaSharing.viber();
+
+window = VanillaSharing.vk();
+
+window = VanillaSharing.whatsapp();
---
+++ vanilla-sharing.d.ts
@@ -0,0 +1,24 @@
+// Type definitions for VanillaSharing
+// Project: vanilla-sharing
+// Definitions by: Alexey Avdeev <https://alexey-avdeev.com/>
+
+export as namespace VanillaSharing;
+
+export function email(...args: any[]): any;
+export function fbButton(...args: any[]): any;
+export function fbFeed(...args: any[]): any;
+export function fbShare(...args: any[]): any;
+export function gp(...args: any[]): any;
+export function line(...args: any[]): any;
+export function linkedin(...args: any[]): any;
+export function mail(...args: any[]): any;
+export function messenger(...args: any[]): any;
+export function ok(...args: any[]): any;
+export function pinterest(...args: any[]): any;
+export function reddit(...args: any[]): any;
+export function telegram(...args: any[]): any;
+export function tumblr(...args: any[]): any;
+export function tw(...args: any[]): any;
+export function viber(...args: any[]): any;
+export function vk(...args: any[]): any;
+export function whatsapp(...args: any[]): any;
